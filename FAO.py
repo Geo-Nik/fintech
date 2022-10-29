0cfc8613ccc63f9f9e01ad72e22bbd47743e8f4f
Volodymyr Nikulishyh <v.nikulishyn@gmail.com>
2022-10-29 22:24:14 +0300

    Refactoring first interation: FAO.py

--- FAO.py
+++ FAO.py
@@ -2,20 +2,6 @@ import openpyxl
 import logging
 
 
-def is_intvalue_in_range(value_, range_):
-    if isinstance(value_, int) and value_ in range_:
-        return True
-    else:
-        return False
-
-
-def is_positive_integer(value_):
-    if isinstance(value_, int) and value_ >= 0:
-        return True
-    else:
-        return False
-
-
 def format_data_in_tuple(tuple_):
     list_ = [
         tuple_item.strip() if isinstance(tuple_item, str) else tuple_item
@@ -24,22 +10,36 @@ def format_data_in_tuple(tuple_):
     return tuple(list_)
 
 
-def validate_range(min_, max_, range_upper_limit):
-    assert is_positive_integer(range_upper_limit)
-    limits_of_range = range(0, range_upper_limit + 1)
-    if not is_intvalue_in_range(min_, limits_of_range):
-        logging.error("The min value exceeds limit")
-        return
-    if max_ is not None and not is_intvalue_in_range(
-        max_, limits_of_range
-    ):
-        logging.error("The max value exceeds limit")
-        return
-    if max_ is None:
-        max_ = range_upper_limit
-    if min_ == max_:
-        return
-    return min_, max_
+def are_ends_of_range_int(ends_of_range: dict):
+    if isinstance(ends_of_range['min'], int) and isinstance(
+                    ends_of_range['max'], int):
+        return True
+    else:
+        logging.error(f"The ends of the range {ends_of_range} are not integer.")
+        return False
+
+
+def are_end_of_range_positive(ends_of_range: dict):
+    if ends_of_range['min'] >= 0 and ends_of_range['max'] >= 0:
+        return True
+    else:
+        return False
+
+
+def is_max_more_than_min(ends_of_range: dict):
+    if ends_of_range['max'] >= ends_of_range['min']:
+        return True
+    else:
+        return False
+
+
+def is_list_contains(List1, List2):
+    set1 = set(List1)
+    set2 = set(List2)
+    if set1.intersection(set2) == set1:
+        return True
+    else:
+        return False
 
 
 class Work_Book:
@@ -79,65 +79,102 @@ class WorkSheet(Work_Book):
         return worksheet
 
 
-class TableRanges(WorkSheet):
+class Range:
+    def __init__(self, auto_ends: dict, custum_ends: dict = None):
+        self.custum_ends = custum_ends
+        self.auto_ends = auto_ends
+
+    def _get_custom_range(self):
+        custom_range = range(self.custum_ends['min'],
+                             self.custum_ends['max'] + 1)
+        auto_range = range(self.auto_ends['min'], self.auto_ends['max'] + 1)
+        if is_list_contains(list(custom_range), list(auto_range)):
+            return self.custum_ends
+        else:
+            logging.warning(f"The custom range [{self.custum_ends['min']},"
+                            f"{self.custum_ends['max']}] exceeds hard limit"
+                            f"[{self.auto_ends['min']},{self.auto_ends['max']}]"
+                            ". Was returned automatic values based on current "
+                            "worksheet instead.")
+            return self.auto_ends
+
+    def set_range(self):
+        assert_comment1 = "The custom range should be positive."
+        assert_comment2 = "The max of range should be more than min."
+        assert are_end_of_range_positive(self.auto_ends), assert_comment1
+        assert is_max_more_than_min(self.auto_ends), assert_comment2
+
+        if are_ends_of_range_int(self.auto_ends):
+            if self.custum_ends is None:
+                return self.auto_ends
+            elif are_ends_of_range_int(self.custum_ends):
+                return self._get_custom_range()
+
+
+class TableRanges:
+    def __init__(self, worksheet, row_col_ranges_dict: dict = None):
+        if row_col_ranges_dict:
+            try:
+                self.row_custom_ends = {
+                                        'min': row_col_ranges_dict["min_row"],
+                                        'max': row_col_ranges_dict["max_row"],
+                                        }
+                self.col_custom_ends = {
+                                        'min': row_col_ranges_dict["min_col"],
+                                        'max': row_col_ranges_dict["max_col"],
+                                        }
+            except KeyError as err:
+                logging.warning("KeyError: The key of row_col_ranges_dict "
+                                f"variable is not correct:{err}. "
+                                "Was returned automatic "
+                                "values based on current worksheet instead.")
+                self.row_custom_ends = None
+                self.col_custom_ends = None
+
+        else:
+            self.row_custom_ends = None
+            self.col_custom_ends = None
+
+        self.row_auto_ends = {
+                              'min': worksheet.min_row,
+                              'max': worksheet.max_row,
+                              }
+        self.col_auto_ends = {
+                              'min': worksheet.min_column,
+                              'max': worksheet.max_column,
+                              }
+
+    def get_ranges(self):
+        row = Range(self.row_auto_ends, self.row_custom_ends)
+        col = Range(self.col_auto_ends, self.col_custom_ends)
+        row_range_dict = row.set_range()
+        col_range_dict = col.set_range()
+        if row_range_dict and col_range_dict:
+            return {'row': row_range_dict, 'col': col_range_dict}
+
+
+class TableData(WorkSheet):
     def __init__(
-        self, path, worksheet_name=None, row_col_ranges_dict: dict = {}
-    ):
+        self, path, worksheet_name=None, row_col_ranges_dict: dict = None
+            ):
+        self.path = path
         self.row_col_ranges_dict = row_col_ranges_dict
         super().__init__(path, worksheet_name)
-        self.worksheet = self.read_worksheet()
-
-    def get_row_col_ranges(self):
-        if not self.row_col_ranges_dict:
-            self.row_col_ranges_dict["min_row"] = 0
-            self.row_col_ranges_dict["max_row"] = None
-            self.row_col_ranges_dict["min_column"] = 0
-            self.row_col_ranges_dict["max_column"] = None
-
-    def validate_row_col_ranges(self):
-        self.get_row_col_ranges()
-        row_ = validate_range(
-            self.row_col_ranges_dict["min_row"],
-            self.row_col_ranges_dict["max_row"],
-            self.worksheet.max_row,
-        )
-        table_ranges_dict = {}
-        if row_:
-            table_ranges_dict["min_row"], table_ranges_dict["max_row"] = row_
-        else:
-            return
-        col_ = validate_range(
-            self.row_col_ranges_dict["min_column"],
-            self.row_col_ranges_dict["max_column"],
-            self.worksheet.max_column,
-        )
-        if col_:
-            table_ranges_dict["min_col"], table_ranges_dict["max_col"] = col_
-        else:
-            return
-        return table_ranges_dict
-
-
-class Data(TableRanges):
-    def __init__(
-        self, path, worksheet_name=None, row_col_ranges_dict: dict = {}
-    ):
-        super().__init__(path, worksheet_name, row_col_ranges_dict)
 
     def get_data_from_worksheet(self):
         worksheet = self.read_worksheet()
         if not worksheet:
             return
 
-        worksheet_ranges = self.validate_row_col_ranges()
+        worksheet_ranges_instance = TableRanges(worksheet, row_col_ranges_dict)
+        worksheet_ranges = worksheet_ranges_instance.get_ranges()
         if not worksheet_ranges:
             return
-
         worksheet_iterator = worksheet.iter_rows(
-            min_row=worksheet_ranges["min_row"],
-            max_row=worksheet_ranges["max_row"],
-            min_col=worksheet_ranges["min_col"],
-            max_col=worksheet_ranges["max_col"],
+            min_row=worksheet_ranges['row']['min'],
+            max_row=worksheet_ranges['row']['max'],
+            min_col=worksheet_ranges['col']['min'],
+            max_col=worksheet_ranges['col']['max'],
             values_only=True,
         )
 
@@ -153,4 +190,9 @@ class Data(TableRanges):
 
 
 if __name__ == "__main__":
-    pass
+    path = "data/accounts.xlsx"
+    worksheet_instance = WorkSheet(path).read_worksheet()
+    row_col_ranges_dict = {} #{"min_row": 1, "max_row": 5, "min_col": 1, "max_col": 3}
+    table_ranges_instance = TableRanges(worksheet_instance, row_col_ranges_dict)
+    table_data_instance = TableData(path)
+    print(table_data_instance.get_data_from_worksheet())
